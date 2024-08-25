e0af1c6e39b4034376808c20f43080929cf61789
SepehrShapouri <110922785+SepehrShapouri@users.noreply.github.com>
2024-08-25 22:34:29 +0330

    feat:added the user tooltip

--- src/components/posts/Post.tsx
+++ src/components/posts/Post.tsx
@@ -4,6 +4,8 @@ import UserAvatar from "../main/UserAvatar";
 import { formatRelativeDate } from "@/lib/utils";
 import { useSession } from "../providers/SessionProvider";
 import PostActions from "./PostActions";
+import Linkify from "../Linkify";
+import UserTooltip from "../users/UserTooltip";
 
 type PostProps = {
   post: PostData;
@@ -14,23 +16,29 @@ export default function Post({ post }: PostProps) {
     <article className="space-y-3 rounded-2xl bg-card p-5 shadow-sm">
       <div className="flex justify-between gap-3">
       <div className="flex flex-wrap gap-3">
+        <UserTooltip user={post.user}>
         <Link href={`/users/${post.user.username}`}>
           <UserAvatar avatarUrl={post.user.avatarUrl} size={40}/>
         </Link>
+        </UserTooltip>
         <div className="">
+          <UserTooltip user={post.user}>
           <Link
             href={`/users/${post.user.username}`}
             className="block font-medium hover:underline"
           >
             {post.user.username}
           </Link>
+          </UserTooltip>
           <Link href={`/posts/${post.id}`} className="block text-sm text-muted-foreground hover:underline">
           {formatRelativeDate(post.createdAt)}</Link>
         </div>
       </div>
       {post.userId == user.id && <PostActions post={post} className=""/>}
       </div>
+      <Linkify>
       <div className="whitespace-pre-line break-words">{post.content}</div>
+      </Linkify>
     </article>
   );
 }
