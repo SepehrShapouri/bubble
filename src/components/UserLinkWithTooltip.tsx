dfd82dca41caa6775de0a98dc98691cabff3b773
SepehrShapouri <110922785+SepehrShapouri@users.noreply.github.com>
2024-10-18 14:05:30 +0330

    fix:replaced server actions for fetching user data on the client with route handlers

--- src/components/UserLinkWithTooltip.tsx
+++ src/components/UserLinkWithTooltip.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { getUser } from "@/app/(main)/users/actions";
+import api from "@/lib/ky";
+import { UserData } from "@/lib/types";
 import { useQuery } from "@tanstack/react-query";
-import { PropsWithChildren } from "react";
-import { useSession } from "./providers/SessionProvider";
 import Link from "next/link";
+import { PropsWithChildren } from "react";
 import UserTooltip from "./users/UserTooltip";
+import { HTTPError } from "ky";
 
 type UserLinkWithTooltipProps = PropsWithChildren & {
   username: string;
@@ -14,9 +15,17 @@ type UserLinkWithTooltipProps = PropsWithChildren & {
 function UserLinkWithTooltip({ username, children }: UserLinkWithTooltipProps) {
   const { data, isLoading } = useQuery({
     queryKey: ["user-data", username],
-    queryFn: async () => await getUser(username),
+    queryFn: () =>
+      api.get(`/api/users/username/${username}`).json<UserData>(),
+    retry(failureCount, error) {
+      if (error instanceof HTTPError && error.response.status === 404) {
+        return false;
+      }
+      return failureCount < 3;
+    },
     staleTime: Infinity,
   });
+
   if (!data) {
     return (
       <Link
